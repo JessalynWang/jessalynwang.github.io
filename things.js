739c0509c2b80315397d10a9eba8d298c12553a3
Jessalyn Wang <jwang456@ucsc.edu>
2020-07-03 01:26:59 -0700

    javascript on seperate file

---
+++ things.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function() {
+    $("#anchor").on('click', function (event) {
+        event.preventDefault();
+        var element = document.getElementById("indexid");
+
+        element.scrollIntoView({ behavior: "smooth" });
+    });
+    $("#anchor1").on('click', function(event) {
+        event.preventDefault();
+        var element = document.getElementById("aboutid");
+
+        element.scrollIntoView({behavior: "smooth"});
+    });
+    $("#anchor2").on('click', function(event) {
+        event.preventDefault();
+        var element = document.getElementById("contactid");
+
+        element.scrollIntoView({behavior: "smooth"});
+    });
+});
