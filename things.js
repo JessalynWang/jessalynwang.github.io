b4384b449b7a8007d1cb67bdcf31a79bc585e95f
Jessalyn Wang <jwang456@ucsc.edu>
2020-07-03 20:15:12 -0700

    animation added

--- things.js
+++ things.js
@@ -5,39 +5,70 @@ $(document).ready(function () {
 
 
     $("#anchor").on('click', function (event) {
-        event.preventDefault();
-        var element = document.getElementById("indexid");
 
-        element.scrollIntoView({ behavior: "smooth" });
+        event.preventDefault();
+
+        setTimeout(
+            function() {
+                var element = document.getElementById("indexid");
+
+                element.scrollIntoView({ behavior: "smooth" });
+            }
+            , 100);
     });
     $("#anchor1").on('click', function(event) {
         event.preventDefault();
-        var element = document.getElementById("aboutid");
 
-        element.scrollIntoView({behavior: "smooth"});
+        setTimeout(
+            function () {
+                var element = document.getElementById("aboutid");
+
+                element.scrollIntoView({ behavior: "smooth" });
+            }
+            , 100);
     });
     $("#anchor2").on('click', function(event) {
         event.preventDefault();
-        var element = document.getElementById("contactid");
 
-        element.scrollIntoView({behavior: "smooth"});
+        setTimeout(
+            function () {
+                var element = document.getElementById("contactid");
+
+                element.scrollIntoView({ behavior: "smooth" });
+            }
+            , 100);
     });
     $("#more").on('click', function (event) {
         event.preventDefault();
-        var element = document.getElementById("moreid");
 
-        element.scrollIntoView({ behavior: "smooth" });
+        setTimeout(
+            function () {
+                var element = document.getElementById("moreid");
+
+                element.scrollIntoView({ behavior: "smooth" });
+            }
+            , 100);
     });
     $("#proj-c").on('click', function (event) {
         event.preventDefault();
-        var element = document.getElementById("proj");
 
-        element.scrollIntoView({ behavior: "smooth" });
+        setTimeout(
+            function () {
+                var element = document.getElementById("proj");
+
+                element.scrollIntoView({ behavior: "smooth" });
+            }
+            , 100);
     });
     $("#proj-c2").on('click', function (event) {
         event.preventDefault();
-        var element = document.getElementById("proj");
 
-        element.scrollIntoView({ behavior: "smooth" });
+        setTimeout(
+            function () {
+                var element = document.getElementById("proj");
+
+                element.scrollIntoView({ behavior: "smooth" });
+            }
+            , 100);
     });
 });
