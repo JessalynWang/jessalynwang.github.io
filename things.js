a41f5a578cd6e1529e62ca71b88596c82dce2e54
Jessalyn Wang <jwang456@ucsc.edu>
2020-07-05 01:47:43 -0700

    formatting articles

--- things.js
+++ things.js
@@ -73,3 +73,30 @@ $(document).ready(function () {
     });
 });
 
+(function loadContent() {
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let articleData = JSON.parse(this.responseText);
+            addContent(articleData);
+        }
+    };
+    let name = location.search.substring(6);
+    xmlhttp.open("GET", "https://jessalynwang.github.io/articles/" + name + ".json", true);
+    xmlhttp.send();
+
+})();
+
+function addContent(articleData) {
+    let titles = document.getElementsByClassName("title");
+    for (let a in titles) titles[a].innerHTML = articleData.title;
+    let authors = document.getElementsByClassName("author");
+    for (let a in authors) authors[a].innerHTML = articleData.authors;
+
+    let contents = document.getElementById("text");
+    let _ = "";
+    for (let a in articleData.content) {
+        _ = _ + articleData.content[a] + "<br>";
+    }
+    contents.innerHTML = _;
+}
